5e0970fdc338c930fb1ef0639189c44851ef0bdd
atharva naik <atharvanaik2018@gmail.com>
2020-10-04 20:17:21 +0530

    finished assignment 1

--- Gno_ML_A1/src/utils.py
+++ Gno_ML_A1/src/utils.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import os
+import copy
 import math
 import glob
 import random
-import pandas as pd 
+import pandas as pd
+import seaborn as sns 
 import matplotlib.pyplot as plt
 
 def loadData(path, verbose=False):
@@ -34,9 +36,10 @@ def loadData(path, verbose=False):
         data[i]['id'] = i
     return data, labels, attr
 
-def gini_index(P):
+def gini_index(Q):
     tot = 1
     test = 0
+    P = copy.deepcopy(Q)
     for p in P:
         test += p
     if test == 0:
@@ -54,31 +57,23 @@ def gini_index(P):
             tot -= p**2
         return tot
 
-def splitData(data, SEED=0, ratio=0.8):
-    size = len(data)
-    train_size = int(ratio*size)
-    test_size = size-train_size
-    random.seed(SEED)
-    indices = sorted(random.sample([i for i in range(size)], test_size))
-    train_data, test_data = [], []
-
-    j = 0
-    for i, datapoint in enumerate(data):
-        if j <test_size:
-            if indices[j] == i:
-                j += 1
-                test_data.append(datapoint)
-            else:
-                train_data.append(datapoint)
-        else:
-            train_data.append(datapoint)
-
-    return train_data, test_data
+def splitData(data, seed=0, val_size=0.2, test_size=0.2):
+    copy_data = copy.deepcopy(data)
+    val_size = int(len(copy_data)*val_size)
+    test_size = int(len(copy_data)*test_size)
+    train_size = len(copy_data) - val_size - test_size
+    
+    random.Random(seed).shuffle(copy_data)
+    train = copy_data[ : train_size]
+    test = copy_data[train_size:train_size + test_size]
+    val = copy_data[train_size + test_size : ]
+    
+    return train, test, val
 
 def cprint(text, r=0, g=255, b=0):
     print("\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text))
 
-def accuracy(data, model, verbose=0):
+def accuracy(data, model, norm=1, verbose=0):
     tot = len(data)
     count = 0
     preds = model.predict(data)
@@ -91,5 +86,24 @@ def accuracy(data, model, verbose=0):
             if verbose:
                 cprint(item['test-type'] + ' , ' + pred[0], r=255, g=0)
 
-    return (count)/tot
-        
+    if norm:
+        return (count)/tot
+    else:
+        return count
+
+def plotDepthVAccuracy(lower_depth, upper_depth, scores, iters_used=10, fname='depth_vs_accuracy_plot'):
+    depth_values = [i for i in range(lower_depth, upper_depth+1)]
+    sns.set(font_scale=1.5)
+    plt.rcParams["figure.figsize"] = (25,17)
+    
+    plt.plot(depth_values, scores)
+    plt.xticks(depth_values, labels=depth_values)
+    plt.xlabel('depth values')
+    plt.ylabel('accuracy')
+    plt.title('Accuracy averaged over {} splits'.format(iters_used))
+    plt.savefig(fname+'.png')
+
+    best_depth = depth_values[scores.index(max(scores))]
+    cprint("plot saved as {}".format(fname+'.png'),r=255,b=100,g=100)
+    cprint("best depth for accuracy averaged over {} iterations is {}".format(iters_used, best_depth),r=255,b=100,g=100)
+    return best_depth
--- Gno_ML_A1/train.py
+++ Gno_ML_A1/train.py
@@ -1,35 +1,97 @@
+import copy
 import pprint
 import random
 import argparse
 from src.model import Tree
-from src.utils import loadData, accuracy, splitData
+from src.utils import loadData, accuracy, splitData, plotDepthVAccuracy, cprint
 
-ITERS = 10
-SPLIT_RATIO = 0.8
-MAX_DEPTH = 12
-DEPTH_UPPER = 7
-DEPTH_LOWER = 5
-PATH = './data/final_dataset.csv'
+## terminal arguments ##
+parser = argparse.ArgumentParser()
+parser.add_argument("-i","--iters", type=int, default=10, help="number of iterations to determine accuracy")
+parser.add_argument("-t","--test_size", type=float, default=0.2, help="ratio of test data to total")
+parser.add_argument("-v","--val_size", type=float, default=0.2, help="ratio of validation data to total")
+parser.add_argument("-p","--path", type=str, default='./data/final_dataset.csv', help="path to dataset")
+parser.add_argument("-u","--upper", type=int, default=16, help="maximum depth value to be explored")
+parser.add_argument("-d","--max_depth", type=int, default=-1, help="maximum depth value of solo decision tree")
+parser.add_argument("-l","--lower", type=int, default=2, help="minimum depth value to be explored")
+args = parser.parse_args()
+
+ITERS = args.iters
+TEST_SIZE = args.test_size
+VAL_SIZE = args.val_size
+MAX_DEPTH = args.max_depth
+DEPTH_UPPER = args.upper
+DEPTH_LOWER = args.lower
+PATH = args.path
 
 data, target, attr = loadData(path=PATH)
 random.seed(1)
-seeds = random.sample([i for i in range(ITERS*10)], ITERS)
-print(seeds)
-model = Tree(attr)
-model.growTree(data, max_d=MAX_DEPTH)
-# pprint.pprint(model.tree, width=1)
-# model.printTree()
+seeds = sorted(random.sample([i for i in range(ITERS*50)], ITERS))
+print("Seeds used for 10 splits are = {}".format(seeds))
+# model = Tree(attr)
+# model.growTree(data, max_d=MAX_DEPTH)
+# # pprint.pprint(model.tree, width=1)
+# # model.printTreeTerminal()
+# print(accuracy(model=model, data=data))
+# # model.printTreeImage()
+cprint("\nDETERMING BEST DEPTH VALUE (PRE PRUNING/ EARLY STOP) ...\n",r=255,b=255,g=0)
 scores = []
-for depth in range(DEPTH_LOWER, DEPTH_UPPER+1):
+best_accuracy, best_depth = 0, DEPTH_LOWER
+best_tree = None
+best_split = {'test':[],'train':[],'val':[]}
+depths = [i for i in range(DEPTH_LOWER, DEPTH_UPPER+1)]
+for depth in depths:
     score = 0
+    best_accuracy_for_this_depth = 0
+    best_tree_for_this_depth = None
+    best_split_for_this_depth = {'test':[],'train':[],'val':[]}
     for seed in seeds:
-        train_data, test_data = splitData(data=data, SEED=seed, ratio=SPLIT_RATIO)
+        train, test, val = splitData(data=data, seed=seed, val_size=VAL_SIZE, test_size=TEST_SIZE)
         temp_model = Tree(attr)
-        temp_model.growTree(train_data, max_d=depth)
-        temp = accuracy(test_data, temp_model)
-        print(temp)
+        temp_model.growTree(train, max_d=depth)
+        temp = accuracy(test, temp_model, norm=1)
+        
+        if best_accuracy_for_this_depth < temp:
+            best_accuracy_for_this_depth = temp
+            best_tree_for_this_depth = temp_model
+            best_split_for_this_depth['train'] = train
+            best_split_for_this_depth['test'] = test
+            best_split_for_this_depth['val'] = val
         score += temp
+    
     score /= ITERS
-    print("depth={}, accuracy={}".format(depth, score))
+    if score >= best_accuracy:
+        best_accuracy = score
+        best_split = best_split_for_this_depth
+        best_depth = depth
+        best_tree = best_tree_for_this_depth
+    cprint("depth={} accuracy={}".format(depth, score), r=255, g=255)
     scores.append(score)
-print(scores)
+
+
+cprint("\nBest depth is {}".format(best_depth),b=255)
+cprint("\nPLOTTING DEPTH VS ACCURACY ...\n",b=100)
+plotDepthVAccuracy(DEPTH_LOWER, DEPTH_UPPER, scores, ITERS)
+cprint("Accuracy of best tree = {}".format(accuracy(best_split['test'], best_tree)), r=255,b=255,g=0)
+# best_tree.prune()
+best_tree.printTreeTerminal()
+cprint("\nSAVING IMAGE OF TREE BEFORE PRUNING ...\n",r=180,b=180,g=180)
+best_tree.printTreeImage()
+cprint("\nPRUNING THE BEST TREE ...",r=255,g=160,b=0)
+best_tree.postPrune(best_split['val'])
+cprint("\nSAVING IMAGE OF TREE AFTER PRUNING ...\n",r=180,b=180,g=180)
+best_tree.printTreeImage('pruned_decision_tree')
+print(accuracy(best_split['val'], best_tree))
+cprint("After pruning accuracy is = {}".format(accuracy(best_split['test'], best_tree)),r=150,g=240,b=30)
+
+
+## Showing a case to validate pruning procedure ##
+cprint("\nA CASE WHERE PRUNING IMPROVES PERFORMANCE\n",r=255,g=0)
+cprint("\ndepth=5",b=255)
+model = Tree(attr)
+model.growTree(best_split['train'], max_d=5)
+print("test set accuracy before pruning = {}".format(accuracy(best_split['test'], model)))
+print("validation set accuracy before pruning = {}".format(accuracy(best_split['val'], model)))
+model.postPrune(best_split['val'])
+print("validation set accuracy after pruning = {}".format(accuracy(best_split['val'], model)))
+print("test set accuracy after pruning = {}".format(accuracy(best_split['test'], model)))
